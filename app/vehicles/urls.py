24c142069c0ab1c172b44ee03e0b96cf939a9df4
Ish Kumar <ish1301@gmail.com>
2024-01-23 14:39:32 -0500

    Load rest framework in settings

--- app/vehicles/urls.py
+++ app/vehicles/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from vehicles.views import VehicleList, VehicleView
 
 urlpatterns = [
-    path("list/", VehicleList.as_view(), name="vehicle_list"),
+    # path("list/", VehicleList.as_view(), name="vehicle_list"),
     path(
         "view/<str:vin>",
         VehicleView.as_view(),
