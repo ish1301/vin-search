1fc1ddaaced309460829de3c493c7522fa2b19d0
Ish Kumar <ish1301@gmail.com>
2024-01-23 03:05:02 -0500

    ignore big files

--- scripts/load_inventory.py
+++ scripts/load_inventory.py
@@ -2,7 +2,7 @@ import csv
 
 csv.register_dialect('piper', delimiter='|', quoting=csv.QUOTE_NONE)
 
-file_path = ''
+file_path = './data/inventory-listing-2022-08-17.txt'
 
 with open(file_path, "rb") as csvfile:
     for row in csv.DictReader(csvfile, dialect='piper'):
