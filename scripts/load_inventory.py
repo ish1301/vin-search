8dc8fb267f7a375d183c3964450828e96fbcddd5
Ish Kumar <ish1301@gmail.com>
2024-01-23 03:07:41 -0500

    Load file

--- scripts/load_inventory.py
+++ scripts/load_inventory.py
@@ -1,9 +1,9 @@
 import csv
 
-csv.register_dialect('piper', delimiter='|', quoting=csv.QUOTE_NONE)
+csv.register_dialect("piper", delimiter="|", quoting=csv.QUOTE_NONE)
 
-file_path = './data/inventory-listing-2022-08-17.txt'
+file_path = "./data/inventory-listing-2022-08-17.txt"
 
 with open(file_path, "rb") as csvfile:
-    for row in csv.DictReader(csvfile, dialect='piper'):
+    for row in csv.DictReader(csvfile, dialect="piper"):
         print(row)
